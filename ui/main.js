370821ef4f551e0e949e9c7de2309ce8bfcfe1c3
palaniappan1 <palaniappan.pp@gmail.com>
2017-05-15 20:01:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,7 @@
-console.log('Loaded!');
-var element=document.getElementById('maintext'
-);
-element.innerHTML='palaniappan';
+var button=document.GetElementByd('counter');
+var counter=0;
+button.onclick=function(){
+  counter=counter+1;
+  var span=document.GetElementById('count');
+  span.innerHTML=counter.toString();    
+};
