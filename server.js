57345e74a71ef0d95cea7986c607f9d82885c17b
palaniappan1 <palaniappan.pp@gmail.com>
2017-05-15 12:03:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/articleone',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html')); 
 });
 app.get('/articletwo',function(req,res){
-   res.send('Article two is requested and will be served'); 
+    res.sendFile(path.join(__dirname, 'ui', 'articletwo.html')); 
 });
 app.get('/articlethree',function(req,res){
-   res.send('Article three is requested and will be served'); 
+     res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));  
 });
 
 app.get('/ui/style.css', function (req, res) {
